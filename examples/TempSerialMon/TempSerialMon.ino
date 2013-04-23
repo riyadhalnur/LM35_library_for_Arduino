40d8668362c776d3f1525452fa16366856c6b857
Riyadh Al Nur <riyadhalnur@verticalaxisbd.com>
2013-04-23 23:45:03 +0600

    Library Uploaded
    The library is ready for use and is ready to be downloaded

---
+++ examples/TempSerialMon/TempSerialMon.ino
@@ -0,0 +1,32 @@
+/* 
+Temperature reading on Serial Monitor using LM35 Analog Linear Temperature Sensor
+
+Uses the LM35 analog sensor to measure temperature. The sketch reads sensor values every
+5 seconds and outputs it on the Serial Monitor. The library can output readings in Celcius and 
+Fahrenheit. 
+
+For the circuit, please visit
+riyadhalnur.wordpress.com
+
+Created on: 23/04/2013
+by Riyadh Al Nur
+the LM35 library copyright (c) 2013 Riyadh Al Nur. All rights reserved.
+*/
+#include <lm35.h> // include the library header
+
+lm35 temps(0); /* create sensor object
+(pin number should correpond to the 
+analog pin the sensor is connected to) */
+
+void setup() {
+  Serial.begin(9600); // setup serial communication
+}
+
+void loop() {
+  temps.MeasureTemp(); // start measuring temperature
+  Serial.print(temps.TempInCelcius); // print temperature
+  Serial.println("C");
+  Serial.print(temps.TempInFahrenheit); // print temp in Fahrenheit
+  Serial.println("F");
+  delay(500); // delay of 5 seconds between readings
+}
---
+++ lm35.cpp
@@ -0,0 +1,41 @@
+/*
+  lm35.h - LM35 Analog Linear Temperature Sensor Library
+  2013 Copyright (c) Riyadh Al Nur.  All rights reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+#include <stdio.h>
+#include <string.h>
+#include <inttypes.h>
+#include "lm35.h"
+
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
+lm35::lm35(int pin)
+{
+  _pin = pin;
+}
+void lm35::MeasureTemp(void)
+{
+  val = analogRead(_pin);
+  dat = (val*500)>>10; // adjust sensor value by 500 (the sensor outputs readings at mV) and adjust for 10mV offset (see datasheet)
+  TempInCelcius = dat;
+  fah = (TempInCelcius * (9.0/5.0)) + 32; // convert celcius to fahrenheit
+  TempInFahrenheit = fah;
+}
